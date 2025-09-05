8ebc21c14710f6f1d9ded5ecdd2ccc338ed55314
juliuscaezarff <julius.caezar25@gmail.com>
2025-09-04 23:40:41 -0400

    refactor(ui): update ProfileMenu with switch and micro interactions

--- src/components/ProfileMenu.tsx
+++ src/components/ProfileMenu.tsx
@@ -1,21 +1,13 @@
-import * as Checkbox from "@radix-ui/react-checkbox";
+import * as Switch from "@radix-ui/react-switch";
 import {
-  CalendarIcon,
-  CheckIcon,
-  CopyIcon,
-  ExitIcon,
   GearIcon,
   HamburgerMenuIcon,
-  LayoutIcon,
   LightningBoltIcon,
-  MoonIcon,
   RowsIcon,
-  SunIcon,
-  ViewHorizontalIcon,
 } from "@radix-ui/react-icons";
 import * as Popover from "@radix-ui/react-popover";
 import * as ToggleGroup from "@radix-ui/react-toggle-group";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { useAtom } from "jotai";
 import { signOut, useSession } from "next-auth/react";
 import { useTheme } from "next-themes";
@@ -32,6 +24,9 @@ import { api } from "~/utils/api";
 import { Hotkey } from "./Hotkey";
 import { Separator } from "./Separator";
 import { Spinner } from "./Spinner";
+import { SunDimIcon } from "./icons/SunDimIcon";
+import { MoonIcon } from "./icons/MoonIcon";
+import { LogoutIcon } from "./icons/LogoutIcon";
 
 export const ProfileMenu = () => {
   const session = useSession();
@@ -148,14 +143,14 @@ export const ProfileMenu = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="rounded-full bg-black/10 p-2 text-black no-underline transition hover:bg-black/20 dark:bg-white/10 dark:text-white dark:dark:hover:bg-white/20"
+          className="rounded-full text-black no-underline dark:text-white dark:dark:hover:bg-white/20"
         >
           <div className="flex items-center gap-x-2 align-middle">
             {session.data?.user?.image ? (
               <Image
                 src={session.data?.user?.image}
-                width={24}
-                height={24}
+                width={32}
+                height={32}
                 className="rounded-full"
                 alt="Profile Picture"
               />
@@ -170,7 +165,7 @@ export const ProfileMenu = () => {
           <motion.div
             initial={{ opacity: 0, y: -4 }}
             animate={{ opacity: 1, y: 0 }}
-            className="mt-4 sm:w-80 w-64 flex flex-col gap-3 rounded-md border border-black/10 bg-black/5 p-4 align-middle font-semibold text-black no-underline backdrop-blur-lg dark:border-white/10 dark:bg-white/5 dark:text-white"
+            className="mt-4 flex w-64 flex-col gap-3 rounded-md border border-black/10 bg-black/5 p-4 align-middle font-semibold text-black no-underline backdrop-blur-lg dark:border-white/10 dark:bg-white/5 dark:text-white sm:w-80"
           >
             <div className="flex items-center gap-2 px-1 align-middle">
               <div className="flex items-center gap-2 align-middle">
@@ -179,37 +174,16 @@ export const ProfileMenu = () => {
               </div>
             </div>
             <Separator />
-            <div className="flex flex-col gap-4 px-1">
-              <div className="flex items-center justify-between gap-x-2 pt-2 align-middle">
-                <div className="flex items-center gap-x-3 align-middle">
-                  <AnimatePresence mode="popLayout">
-                    {viewStyle === "compact" ? (
-                      <motion.div
-                        key="compact"
-                        initial={{ opacity: 0, scale: 0.9 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        exit={{ opacity: 0, scale: 0.9 }}
-                      >
-                        <HamburgerMenuIcon className="h-4 w-4 text-gray-800 dark:text-gray-400" />
-                      </motion.div>
-                    ) : (
-                      <motion.div
-                        key="expanded"
-                        initial={{ opacity: 0, scale: 0.9 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        exit={{ opacity: 0, scale: 0.9 }}
-                      >
-                        <RowsIcon className="h-4 w-4 text-gray-800 dark:text-gray-400" />
-                      </motion.div>
-                    )}
-                  </AnimatePresence>
+            <div className="space-y-0.5">
+              <div className="flex h-8 items-center justify-between">
+                <div className="flex items-center gap-2">
                   <p className="text-sm font-normal">View</p>
                   <Hotkey key1="v" />
                 </div>
                 <ToggleGroup.Root
                   type="single"
                   defaultValue={viewStyle}
-                  className="flex items-center gap-x-2 align-middle"
+                  className="flex h-6 items-center gap-x-1 rounded-md bg-black/5 p-0.5 dark:bg-white/5"
                   onValueChange={(value) => {
                     if (value !== viewStyle && value !== "") {
                       handleChangeViewStyle(value as "compact" | "expanded");
@@ -218,65 +192,28 @@ export const ProfileMenu = () => {
                 >
                   <ToggleGroup.Item
                     value="compact"
-                    className="flex items-center gap-x-2 align-middle"
+                    className="flex h-5 w-6 items-center justify-center rounded data-[state=on]:bg-black/20 data-[state=on]:shadow-sm dark:data-[state=on]:bg-white/20"
                   >
-                    <p
-                      className={`text-sm transition duration-200 ease-in-out hover:text-black dark:hover:text-white ${
-                        viewStyle === "compact"
-                          ? "font-semibold"
-                          : "font-normal text-zinc-500"
-                      }`}
-                    >
-                      Compact
-                    </p>
+                    <HamburgerMenuIcon className="h-3 w-3 text-zinc-700 data-[state=on]:text-zinc-700 dark:text-gray-400 dark:data-[state=on]:text-zinc-700" />
                   </ToggleGroup.Item>
                   <ToggleGroup.Item
                     value="expanded"
-                    className="flex items-center gap-x-2 align-middle"
+                    className="flex h-5 w-6 items-center justify-center rounded data-[state=on]:bg-black/20 data-[state=on]:shadow-sm dark:data-[state=on]:bg-white/20"
                   >
-                    <p
-                      className={`text-sm transition duration-200 ease-in-out hover:text-black dark:hover:text-white ${
-                        viewStyle === "expanded"
-                          ? "font-semibold"
-                          : "font-normal text-zinc-500"
-                      }`}
-                    >
-                      Expanded
-                    </p>
+                    <RowsIcon className="h-3 w-3 text-zinc-700 data-[state=on]:text-zinc-700 dark:text-gray-400 dark:data-[state=on]:text-zinc-700" />
                   </ToggleGroup.Item>
                 </ToggleGroup.Root>
               </div>
 
-              <div className="flex flex-row justify-between align-middle">
-                <div className="flex items-center gap-x-3 align-middle">
-                  <AnimatePresence mode="popLayout">
-                    {resolvedTheme === "light" ? (
-                      <motion.div
-                        key="light"
-                        initial={{ opacity: 0, scale: 0.9 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        exit={{ opacity: 0, scale: 0.9 }}
-                      >
-                        <SunIcon className="h-4 w-4 text-gray-800 dark:text-gray-400" />
-                      </motion.div>
-                    ) : (
-                      <motion.div
-                        key="dark"
-                        initial={{ opacity: 0, scale: 0.9 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        exit={{ opacity: 0, scale: 0.9 }}
-                      >
-                        <MoonIcon className="h-4 w-4 text-gray-800 dark:text-gray-400" />
-                      </motion.div>
-                    )}
-                  </AnimatePresence>
-                  <p className="text-sm font-normal">Day or night</p>
+              <div className="flex h-8 items-center justify-between">
+                <div className="flex items-center gap-2">
+                  <p className="text-sm font-normal">Theme</p>
                   <Hotkey key1="t" />
                 </div>
                 <ToggleGroup.Root
                   type="single"
                   defaultValue={resolvedTheme}
-                  className="flex items-center gap-x-2 align-middle"
+                  className="flex h-6 items-center gap-x-1 rounded-md bg-black/5 p-0.5 dark:bg-white/5"
                   onValueChange={(value) => {
                     if (value !== resolvedTheme && value !== "") {
                       handleChangeTheme(value as "light" | "dark");
@@ -285,161 +222,76 @@ export const ProfileMenu = () => {
                 >
                   <ToggleGroup.Item
                     value="light"
-                    className="flex items-center gap-x-2 align-middle"
+                    className="flex h-5 w-6 items-center justify-center rounded data-[state=on]:bg-black/20 data-[state=on]:shadow-sm dark:data-[state=on]:bg-white/20"
                   >
-                    <p
-                      className={`text-sm transition duration-200 ease-in-out hover:text-black dark:hover:text-white ${
-                        resolvedTheme === "light"
-                          ? "font-semibold"
-                          : "font-normal text-zinc-500"
-                      }`}
-                    >
-                      Day
-                    </p>
+                    <SunDimIcon size={16} />
                   </ToggleGroup.Item>
                   <ToggleGroup.Item
                     value="dark"
-                    className="flex items-center gap-x-2 align-middle"
+                    className="flex h-5 w-6 items-center justify-center rounded data-[state=on]:bg-black/20 data-[state=on]:shadow-sm dark:data-[state=on]:bg-white/20"
                   >
-                    <p
-                      className={`text-sm transition duration-200 ease-in-out hover:text-black dark:hover:text-white ${
-                        resolvedTheme === "dark"
-                          ? "font-semibold"
-                          : "font-normal text-zinc-500"
-                      }`}
-                    >
-                      Night
-                    </p>
+                    <MoonIcon size={16} />
                   </ToggleGroup.Item>
                 </ToggleGroup.Root>
               </div>
 
-              <div className="flex items-center justify-between gap-x-2 align-middle">
-                <div className="flex items-center gap-x-3 align-middle">
-                  <AnimatePresence mode="popLayout">
-                    {showMonths ? (
-                      <motion.div
-                        key="showMonths"
-                        initial={{ opacity: 0, scale: 0.9 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        exit={{ opacity: 0, scale: 0.9 }}
-                      >
-                        <CalendarIcon className="h-4 w-4 text-gray-800 dark:text-gray-400" />
-                      </motion.div>
-                    ) : (
-                      <motion.div
-                        key="notShowMonths"
-                        initial={{ opacity: 0, scale: 0.9 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        exit={{ opacity: 0, scale: 0.9 }}
-                      >
-                        <LayoutIcon className="h-4 w-4 rotate-90 text-gray-800 dark:text-gray-400" />
-                      </motion.div>
-                    )}
-                  </AnimatePresence>
+              <div className="flex h-8 items-center justify-between">
+                <div className="flex items-center gap-2">
                   <p className="text-sm font-normal">Show months?</p>
                   <Hotkey key1="m" />
                 </div>
-                <Checkbox.Root
+                <Switch.Root
                   defaultChecked={showMonths}
-                  className="flex h-6 w-6 items-center justify-center rounded-md bg-black/10 transition  duration-200 ease-in-out hover:bg-black/20 dark:bg-white/10 dark:hover:bg-white/20 "
                   onCheckedChange={() => {
                     handleUpdateShowMonths();
                   }}
+                  className="relative inline-flex h-5 w-9 items-center rounded-full bg-black/5 p-0.5 transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 data-[state=checked]:border-blue-600 data-[state=checked]:bg-blue-600 dark:bg-white/5 dark:data-[state=checked]:border-blue-500 dark:data-[state=checked]:bg-blue-500"
                 >
-                  <motion.div
-                    whileTap={{
-                      scale: 0.8,
-                    }}
-                  >
-                    <Checkbox.Indicator>
-                      <CheckIcon className="h-4 w-4" />
-                    </Checkbox.Indicator>
-                  </motion.div>
-                </Checkbox.Root>
+                  <Switch.Thumb className="inline-block h-4 w-4 transform rounded-full border border-neutral-300 bg-white shadow-sm transition-transform data-[state=checked]:translate-x-4 dark:border-neutral-500" />
+                </Switch.Root>
               </div>
 
-              <div className="flex items-center justify-between gap-x-2 align-middle">
-                <div className="flex items-center gap-x-3 align-middle">
-                  <AnimatePresence mode="popLayout">
-                    {currentFolder?.allowDuplicate ? (
-                      <motion.div
-                        key="allowDuplicate"
-                        initial={{ opacity: 0, scale: 0.9 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        exit={{ opacity: 0, scale: 0.9 }}
-                      >
-                        <CopyIcon className="h-4 w-4 text-gray-800 dark:text-gray-400" />
-                      </motion.div>
-                    ) : (
-                      <motion.div
-                        key="notAllowDuplicate"
-                        initial={{ opacity: 0, scale: 0.9 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        exit={{ opacity: 0, scale: 0.9 }}
-                      >
-                        <ViewHorizontalIcon className="h-4 w-4 text-gray-800 dark:text-gray-400" />
-                      </motion.div>
-                    )}
-                  </AnimatePresence>
+              <div className="flex h-8 items-center justify-between">
+                <div className="flex items-center gap-2">
                   <p className="text-sm font-normal">Allow duplicates?</p>
                   <Hotkey key1="d" />
                 </div>
-                <Checkbox.Root
+                <Switch.Root
                   defaultChecked={currentFolder?.allowDuplicate}
-                  className="flex h-6 w-6 items-center justify-center rounded-md bg-black/10 transition  duration-200 ease-in-out hover:bg-black/20 dark:bg-white/10 dark:hover:bg-white/20 "
                   onCheckedChange={() => {
                     handleUpdateFolder();
                   }}
+                  className="relative inline-flex h-5 w-9 items-center rounded-full bg-black/5 p-0.5 transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 data-[state=checked]:border-blue-600 data-[state=checked]:bg-blue-600 dark:bg-white/5 dark:data-[state=checked]:border-blue-500 dark:data-[state=checked]:bg-blue-500"
                 >
-                  <motion.div
-                    whileTap={{
-                      scale: 0.95,
-                    }}
-                  >
-                    <Checkbox.Indicator>
-                      <CheckIcon className="h-4 w-4" />
-                    </Checkbox.Indicator>
-                  </motion.div>
-                </Checkbox.Root>
+                  <Switch.Thumb className="inline-block h-4 w-4 transform rounded-full border border-neutral-300 bg-white shadow-sm transition-transform data-[state=checked]:translate-x-4 dark:border-neutral-500" />
+                </Switch.Root>
               </div>
 
-              <div className="flex items-center justify-between gap-x-2 pb-1 align-middle">
-                <div className="flex items-center gap-x-3 align-middle">
-                  <ExitIcon className="h-4 w-4 text-gray-800 dark:text-gray-400" />
-                  <p className="text-sm font-normal">Sign out</p>
-                </div>
-                <motion.button
-                  whileTap={{
-                    scale: 0.95,
-                  }}
-                  disabled={signinOut}
-                  onClick={handleSignOut}
-                  className={`flex h-6 w-6 items-center justify-center rounded-md bg-black/10 font-semibold text-black no-underline transition ease-in-out hover:bg-black/20 dark:bg-white/10 dark:text-white dark:hover:bg-white/20  `}
-                >
-                  {signinOut ? (
-                    <Spinner size="sm" />
-                  ) : (
-                    <p className="text-xs font-normal ">{"->"}</p>
-                  )}
-                </motion.button>
-              </div>
+              <motion.button
+                whileTap={{
+                  scale: 0.95,
+                }}
+                disabled={signinOut}
+                onClick={handleSignOut}
+                className="flex h-8 w-full items-center justify-start gap-2 rounded-md px-2 text-sm font-normal text-black hover:bg-black/20 dark:text-white dark:hover:bg-white/20"
+              >
+                <LogoutIcon size={16} />
+                Sign out
+                {signinOut && <Spinner size="sm" />}
+              </motion.button>
             </div>
             <Separator />
-            <div className="flex items-center gap-x-3 px-1 align-middle">
-              <LightningBoltIcon className="h-4 w-4 text-zinc-500 " />
-
-              <div className="flex flex-col gap-1.5">
-                <p className="text-sm font-semibold">Pro tip </p>
-                <div className="flex items-center gap-1.5 align-middle">
-                  <span className="text-sm font-normal text-zinc-500 ">
-                    Press
-                  </span>
-                  <Hotkey key1="f" />
-                  <p className="text-sm font-normal text-zinc-500">
-                    to open Folders
-                  </p>
-                </div>
+            <div className="flex items-start gap-2 rounded-md bg-black/5 px-2 py-2 text-xs dark:bg-white/5">
+              <LightningBoltIcon className="mt-0.5 h-3 w-3 flex-shrink-0" />
+              <p className="text-sm font-normal">Pro tip </p>
+              <div className="flex items-center gap-1.5 align-middle">
+                <span className="text-sm font-normal text-zinc-500 ">
+                  Press
+                </span>
+                <Hotkey key1="f" />
+                <p className="text-sm font-normal text-zinc-500">
+                  to open Folders
+                </p>
               </div>
             </div>
           </motion.div>
